6351af251c0ef0cc63e2ba2ed53d449ba4712ad1
InsomniaKitten <admin@designdevils.co.uk>
2018-07-26 13:52:45 +0100

    Migrate version handling to properties file

--- src/main/kotlin/net/cinnamon/common/Cinnamon.kt
+++ src/main/kotlin/net/cinnamon/common/Cinnamon.kt
@@ -5,17 +5,9 @@ import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-@Mod(
-    modid = Cinnamon.ID,
-    name = Cinnamon.NAME,
-    version = Cinnamon.VERSION,
-    acceptedMinecraftVersions = "[1.12,1.13)",
-    modLanguageAdapter = "CinnamonKotlinAdapter"
-)
+@Mod(modid = Cinnamon.ID, modLanguageAdapter = "CinnamonKotlinAdapter", useMetadata = true)
 object Cinnamon : Logger by LogManager.getLogger("cinnamon") {
     const val ID = "cinnamon"
-    const val NAME = "Cinnamon"
-    const val VERSION = "@cinnamon.version@"
 
     val isDeobfuscated by lazy {
         Launch.blackboard["fml.deobfuscatedEnvironment"] as Boolean
