d24c277a8d6412d9295a5a1c776087a212a523a6
InsomniaKitten <admin@designdevils.co.uk>
2018-07-29 02:59:27 +0100

    Fix nullability handling in JsonPrimitives

--- src/main/kotlin/net/cinnamon/common/serializable/json/JsonPrimitives.kt
+++ src/main/kotlin/net/cinnamon/common/serializable/json/JsonPrimitives.kt
@@ -2,6 +2,8 @@
 
 package net.cinnamon.common.serializable.json
 
+import com.google.gson.JsonElement
+import com.google.gson.JsonNull
 import com.google.gson.JsonPrimitive
 
 /**
@@ -10,7 +12,7 @@ import com.google.gson.JsonPrimitive
  * @since 0.1.0
  */
 @JvmName("newJsonPrimitive")
-fun Boolean?.toJson() = JsonPrimitive(this)
+fun Boolean?.toJson(): JsonElement = this?.let(::JsonPrimitive) ?: JsonNull.INSTANCE
 
 /**
  * Converts the Number receiver to a [JsonPrimitive]
@@ -18,7 +20,7 @@ fun Boolean?.toJson() = JsonPrimitive(this)
  * @since 0.1.0
  */
 @JvmName("newJsonPrimitive")
-fun Number?.toJson() = JsonPrimitive(this)
+fun Number?.toJson(): JsonElement = this?.let(::JsonPrimitive) ?: JsonNull.INSTANCE
 
 /**
  * Converts the String receiver to a [JsonPrimitive]
@@ -26,7 +28,7 @@ fun Number?.toJson() = JsonPrimitive(this)
  * @since 0.1.0
  */
 @JvmName("newJsonPrimitive")
-fun String?.toJson() = JsonPrimitive(this)
+fun String?.toJson(): JsonElement = this?.let(::JsonPrimitive) ?: JsonNull.INSTANCE
 
 /**
  * Converts the Char receiver to a [JsonPrimitive]
@@ -34,4 +36,4 @@ fun String?.toJson() = JsonPrimitive(this)
  * @since 0.1.0
  */
 @JvmName("newJsonPrimitive")
-fun Char?.toJson() = JsonPrimitive(this)
+fun Char?.toJson(): JsonElement = this?.let(::JsonPrimitive) ?: JsonNull.INSTANCE
